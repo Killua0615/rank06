b072758b99f0c4721e72f94c12b4fe95297871be
Killua <106412328+Killua0615@users.noreply.github.com>
2026-07-17 18:00:53 +0900

    add 1

--- mini_db_1/mini_db.cpp
+++ mini_db_1/mini_db.cpp
@@ -1,15 +1,15 @@
 #include "mini_db.hpp"
 
-std::string g_filesname;
-std::map<std::string, std:: string> g_db;
+std::string g_filename;
+std::map<std::string, std::string> g_db;
 int g_sockfd = -1;
 
-void sava_db_and_exit(int sig) {
+void save_db_and_exit(int sig) {
   (void)sig;
   std::ofstream file(g_filename.c_str());
   if(file.is_open()) {
     std::map<std::string, std::string>::iterator it;
-    for (it = g_db.begin(); it != g_db.end(); ++it)
+    for(it = g_db.begin(); it != g_db.end(); ++it)
       file << it->first << " " << it->second << "\n";
     file.close();
   }
@@ -20,4 +20,14 @@ void sava_db_and_exit(int sig) {
 
 void send_res(int fd, const std::string &msg) {
   send(fd, msg.c_str(), msg.length(), 0);
+}
+
+void load_db(void) {
+  std::ifstream infile(g_filename.c_str());
+  if(!infile.is_open())
+    return;
+  std::string key, val;
+  while(infile >> key >> val)
+    g_db[key] = val;
+  infile.close();
 }
